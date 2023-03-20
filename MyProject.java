403ead825abcc0cd29f0ab122388b6591c76a526
methus <>
2023-03-20 14:56:21 +0700

    modified file

--- MyProject.java
+++ MyProject.java
@@ -3,7 +3,7 @@ package myproject;
 public class MyProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Worlds!");
     }
     
 }
